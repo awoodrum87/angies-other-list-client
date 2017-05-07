98349243dbf6ba35f41bfe498387a4f42b634beb
awoodrum87 <awoodrum87@gmail.com>
2017-05-07 16:28:46 -0400

    updated ui to have a navbar fixed hide/show issues

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -16,22 +16,6 @@ const loginSuccess = (data) => {
   store.user = data.user
   $('#initiateLogin').modal('toggle')
   visible.loginView()
-  // $('.welcome-page').hide()
-  // $('#initiate-login').hide()
-  // $('#get-started').hide()
-  // $('#legal-statement').hide()
-  // $('#logout').show()
-  // $('#ch-password').show()
-  // $('#cr-profile').show()
-  // $('#delete-review').show()
-  // $('#update-profile').show()
-  // $('#create-review').show()
-  // $('#update-review').show()
-  // $('#show-my-reviews').show()
-  // $('#show-all-reviews').show()
-  // $('#up-profile').show()
-  // $('.create-review').show()
-  // $('#retail-image').show()
 }
 
 const loginFailure = (error) => {
@@ -43,24 +27,6 @@ const logoutSuccess = (data) => {
   store.user = null
   console.log('logout out sucess')
   visible.logoutView()
-  // $('#update-profile').hide()
-  // $('.welcome-page').show()
-  // $('#logout').hide()
-  // $('#ch-password').hide()
-  // $('#cr-profile').hide()
-  // $('#initiate-login').show()
-  // $('#get-started').show()
-  // $('#legal-statement').show()
-  // $('#update-review').hide()
-  // $('#ch-password').hide()
-  // $('#cr-profile').hide()
-  // $('#delete-review').hide()
-  // $('#create-review').hide()
-  // $('#show-my-reviews').hide()
-  // $('#show-all-reviews').hide()
-  // $('.create-review').hide()
-  // $('.content').hide()
-  // $('#update-profile').hide()
 }
 
 const logoutFailure = (error) => {
--- assets/scripts/visible.js
+++ assets/scripts/visible.js
@@ -2,43 +2,20 @@
 
 // when a user logs out, hide elements of the page
 const logoutView = function () {
-  $('#update-profile').hide()
-  $('.welcome-page').show()
-  $('#logout').hide()
-  $('#ch-password').hide()
-  $('#cr-profile').hide()
-  $('#initiate-login').show()
-  $('#get-started').show()
-  $('#legal-statement').show()
-  $('#update-review').hide()
-  $('#ch-password').hide()
-  $('#cr-profile').hide()
-  $('#delete-review').hide()
-  $('#create-review').hide()
-  $('#show-my-reviews').hide()
-  $('#show-all-reviews').hide()
+  $('.reviewActions').hide()
+  $('.profileActions').hide()
   $('.create-review').hide()
-  $('.content').hide()
+  $('.accountActions').show()
+  $('.welcome-page').show()
 }
 
 // when a user logs in, show the following elements
 const loginView = function () {
+  $('.accountActions').hide()
   $('.welcome-page').hide()
-  $('#initiate-login').hide()
-  $('#get-started').hide()
-  $('#legal-statement').hide()
-  $('#logout').show()
-  $('#ch-password').show()
-  $('#cr-profile').show()
-  $('#delete-review').show()
-  $('#update-profile').show()
-  $('#create-review').show()
-  $('#update-review').show()
-  $('#show-my-reviews').show()
-  $('#show-all-reviews').show()
-  $('#up-profile').show()
+  $('.reviewActions').show()
+  $('.profileActions').show()
   $('.create-review').show()
-  $('#retail-image').show()
 }
 module.exports = {
   logoutView,
